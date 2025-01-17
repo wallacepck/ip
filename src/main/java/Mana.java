ff45c45f6c5351cc0f1af32f32028b3658c69816
wallacepck <142145699+wallacepck@users.noreply.github.com>
2025-01-17 17:21:37 +0800

    Add greeting, reformat logo

--- src/main/java/Mana.java
+++ src/main/java/Mana.java
@@ -1,13 +1,21 @@
 public class Mana {
+    private static final String BAR = "____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = """
-                 __  __                  \s
+                  __  __                  \s
                  |  \\/  |                 \s
                  | \\  / | __ _ _ __   __ _\s
                  | |\\/| |/ _` | '_ \\ / _` |
                  | |  | | (_| | | | | (_| |
                  |_|  |_|\\__,_|_| |_|\\__,_|\
+                 \n
                 """;
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(BAR);
+        System.out.println("Hello! I'm \n" + logo + "What's up today?");
+        System.out.println(BAR);
+        System.out.println("Bye!");
+        System.out.println(BAR);
     }
 }
