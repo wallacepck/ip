fb035dde96365a071571e1c1ca4e904547407380
wallacepck <142145699+wallacepck@users.noreply.github.com>
2025-01-17 17:17:30 +0800

    Rename main class, logo

---
+++ src/main/java/Mana.java
@@ -0,0 +1,13 @@
+public class Mana {
+    public static void main(String[] args) {
+        String logo = """
+                 __  __                  \s
+                 |  \\/  |                 \s
+                 | \\  / | __ _ _ __   __ _\s
+                 | |\\/| |/ _` | '_ \\ / _` |
+                 | |  | | (_| | | | | (_| |
+                 |_|  |_|\\__,_|_| |_|\\__,_|\
+                """;
+        System.out.println("Hello from\n" + logo);
+    }
+}
